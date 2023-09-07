fd20333725b39a253d4e1ffdf505ae281ea72360
Lucas <Lucas_Ferreira2019@outlook.com>
2023-09-07 15:48:23 -0300

    modificando o myprojects

--- src/components/WppContact.tsx
+++ src/components/WppContact.tsx
@@ -1,7 +1,7 @@
 export const WppContact = ()=>{
     return(
-        <div className="fixed">
-            <img src="/assets/wpp.png" alt="" />
+        <div className="fixed bottom-0 right-0 top-0">
+            <img className="w-[100px]" src="/assets/wpp.png" />
         </div>
     )
 }
