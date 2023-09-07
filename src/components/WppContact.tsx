ae127500b2a880bc76fd5f697a5599ca9da18bcf
Lucas <Lucas_Ferreira2019@outlook.com>
2023-09-07 16:12:05 -0300

    modificando o wpp

--- src/components/WppContact.tsx
+++ src/components/WppContact.tsx
@@ -1,7 +1,10 @@
 export const WppContact = ()=>{
     return(
-        <div className="fixed bottom-0 right-0 top-0">
-            <img className="w-[100px]" src="/assets/wpp.png" />
+        <div className="fixed bottom-10 cursor-pointer right-10 ">
+            <a href="https://wa.me/5511979690639">
+                <img className="w-[50px]" src="/assets/wpp.png" />
+            </a>
+            
         </div>
     )
 }
