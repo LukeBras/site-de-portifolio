c36ebf52dd2180e76c51bed1efa34c445d628ca5
Lucas <Lucas_Ferreira2019@outlook.com>
2024-01-18 11:02:29 -0300

    commitando

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -3,19 +3,19 @@ import { Title } from "./Title"
 
 export  const MyProjects = ()=>{
     return(
-        <div className='h-[900px] mt-[100px]  flex flex-col justify-center items-center mb-[100px] '>
+        <div className='h-[1300px] mt-[300px]  flex flex-col justify-center items-center mb-[200px] '>
             <div className="max-w-[1700px] p-6  justify-between flex flex-wrap  ">
                 <Title text="Projetos"/>
                  <div className=" w-auto  flex flex-wrap justify-between  mt-24  m-auto ">
-                    <a href="https://github.com/LukeBras/Livraria" target="blank">
-                         <Project img="/assets/img1.jpeg" projeto="Livraria"/>
-                    </a>
+                    <Project link="https://github.com/LukeBras/Livraria" img="/assets/img1.jpeg" projeto="Livraria"/>
+                    <Project link="" img="/assets/estacionamento.jpg" projeto="Estacionamento"/>
+                    <Project link="https://github.com/LukeBras/to-do-list" img="/assets/todo.jpeg" projeto="Todo-list"/>
+                    <Project link="https://github.com/LukeBras/galeria-de-fotos" img="/assets/galeria.jpeg" projeto="Galeria de Fotos (Firebase)"/>
+                    <Project link="https://github.com/LukeBras/lading-page-carros" img="/assets/site-carros.jpeg" projeto="Template de Carro"/>
+                    <Project link="https://github.com/LukeBras/clone-do-spotify" img="/assets/spotify.jpeg" projeto="clone Spotify"/>
+                    <Project link="" img="/assets/blog.jpeg" projeto="Blog (context) "/>
+                    <Project link="https://github.com/LukeBras/clone-do-Ifood" img="/assets/ifood.jpeg" projeto="Clone do Ifood "/>
                     
-                    <Project img="/assets/estacionamento.jpg" projeto="Estaciomento"/>
-                    <Project img="/assets/todo.jpeg" projeto="Todo-list"/>
-                    <Project img="/assets/galeria.jpeg" projeto="Galeria de Fotos (Firebase)"/>
-                    <Project img="/assets/site-carros.jpeg" projeto="Site de Carro"/>
-                    <Project img="/assets/blog.jpeg" projeto="Blog (context) "/>
 
                 </div>
             </div>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,17 +1,18 @@
 type ProjectProps = {
     img:string;
-    projeto:string
+    projeto:string;
+    link:string;
 }
 
-export const Project = ({img,projeto}:ProjectProps)=>{
+export const Project = ({img,projeto,link}:ProjectProps)=>{
     return(
         <div>
-             <div className="w-[500px] h-[500px] border hover:bg-gray-200 transition-all border-gray-500 rounded-3xl flex flex-col p-7 m-6 justify-center items-center ">
+             <div className="w-[500px]  border hover:bg-gray-200 transition-all border-gray-500 rounded-3xl flex flex-col p-7 m-6 justify-center items-center ">
                     <img className="max-h-[700px] h-full object-contain border border-gray-400 p-3 rounded-md w-[600px]" src={img} alt="" />
                     <h1 className="text-3xl p-4 font-bold">{projeto}</h1>
                     <div className=" flex justify-between ">
-                        <button className="w-[200px] hover:bg-gray-400 transition-all rounded-lg bg-gray-500 m-3 p-3 text-white">GIT</button>
-                        <button className="w-[200px] hover:bg-gray-400 transition-all rounded-lg bg-gray-500 m-3 p-3 text-white">Acessar</button>
+                        <a href={link} target="blank" className="w-[200px] text-center hover:bg-gray-400 transition-all rounded-lg bg-gray-500 m-3 p-3 text-white">GIT</a>
+                        <a href={link} className="w-[200px] text-center hover:bg-gray-400 transition-all rounded-lg bg-gray-500 m-3 p-3 text-white">Acessar</a>
                     </div>
                 </div>
         </div>
